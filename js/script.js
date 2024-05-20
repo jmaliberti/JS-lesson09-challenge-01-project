922e2ec7751262ef5ec9717e32ab26e3727b638a
Jessica Aliberti <jmaliberti@gmail.com>
2024-05-20 12:49:24 -0700

    Added hide to classList of addGuestButton, guestInput, & guestInputLabel, and removed hide from guestFull class.

--- js/script.js
+++ js/script.js
@@ -34,5 +34,11 @@ const addToList = function (guest) {
 const updateGuestCount = function () {
     let guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText = guests.length;
+    if (guests.length === "8") {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
 
 };
